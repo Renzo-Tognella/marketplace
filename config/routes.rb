fdda272ceecef7c0de6711d40abb948f03b8a581
Renzo Tognella De Rosa <renzoderosa.tognella@gmail.com>
2024-10-16 13:52:19 -0300

    setup inicial

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@
 Rails.application.routes.draw do
   mount Rswag::Api::Engine => '/api-docs'
   mount Rswag::Ui::Engine => '/api-docs'
+  devise_for :users
 
-  namespace :api, defaults: { format: :json },
-                  constraints: { subdoamin: 'api' }, path: '/' do
+  namespace :api, defaults: { format: :json }, constraints: { subdoamin: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      resources :users, only: [:show]
     end
   end
 end
