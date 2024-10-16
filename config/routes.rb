94ca94ed7bd71d03369a341080258bd97db022e4
Renzo Tognella De Rosa <renzoderosa.tognella@gmail.com>
2024-10-16 13:24:01 -0300

    Adds shows action the users controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   mount Rswag::Api::Engine => '/api-docs'
   mount Rswag::Ui::Engine => '/api-docs'
+  devise_for :users
 
-  namespace :api, defaults: { format: :json },
-                  constraints: { subdoamin: 'api' }, path: '/' do
+  namespace :api, defaults: { format: :json }, constraints: { subdoamin: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      resources :users, only: [:show]
     end
   end
 end
