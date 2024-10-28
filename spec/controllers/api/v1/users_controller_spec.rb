d034a2430a8c91bea092a5733ed5e20a1bad7d2d
Renzo Tognella De Rosa <renzoderosa.tognella@gmail.com>
2024-10-28 09:10:12 -0300

    Refactors test headers for each request

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
-  before(:each) do
-    request.headers['Accept'] = 'application/vnd.marketplace.v1'
-  end
   describe 'GET /show' do
     before(:each) do
       @user = FactoryBot.create :user
@@ -12,7 +9,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     end
 
     it 'returns the informating about a reporter on a hash' do
-      user_response = JSON.parse(response.body, symbolize_names: true)
+      user_response = json_response
       expect(user_response[:email]).to eq @user.email
     end
 
@@ -27,7 +24,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       end
 
       it 'renders the json represetation for the user record just created' do
-        user_response = JSON.parse(response.body, symbolize_names: true) 
+        user_response = json_response 
         expect(user_response[:email]).to eq @user_attributes[:email]
       end
 
@@ -45,15 +42,67 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       end
 
       it 'renders an error json' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
+        user_response = json_response
         expect(user_response).to have_key(:errors)
       end
 
 
       it 'renders the json error on why the user could not be created' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
+        user_response = json_response
         expect(user_response[:errors][:email]).to include "can't be blank"
       end
     end
+  end
+
+  describe 'PUT/PATCH #update' do
+    context 'when is successfully updated' do      
+      before(:each) do
+        @user = FactoryBot.create(:user)
+        patch :update, params: {
+          id: @user.id,
+          user: { email: 'newemail@test.com' }, format: :json
+        }
+      end
+
+      it 'renders the json representation for the updated user' do
+        user_response = json_response
+        expect(user_response[:email]).to eql('newemail@test.com')
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context 'when is not updated' do
+      before(:each) do
+        @user = FactoryBot.create(:user)
+        patch :update, params: {
+          id: @user.id,
+          user: { email: 'bademail.com' }, format: :json
+        }
+      end
+
+      it 'rendered an errors json' do
+        user_response = json_response
+
+        expect(user_response).to have_key(:errors)
+      end
+
+      it 'rendered the jsons errors on why the user could not be updated' do
+        user_response = json_response
+
+        expect(user_response[:errors][:email]).to include 'is invalid'
+      end
+
+      it { should respond_with 422}
+    end
+  end
+
+  describe 'DELETE #destroy' do 
+    before(:each) do
+      @user = FactoryBot.create(:user)
+      delete :destroy, params: { id: @user.id, format: :json }
+    end
+
+    it { should respond_with 204 }
   end
 end
