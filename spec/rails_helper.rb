1613e210bf1fa35878825fe988d40a41bd852459
Renzo Tognella De Rosa <renzoderosa.tognella@gmail.com>
2024-11-14 11:42:17 -0300

    Destroy session

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+# Este arquivo é copiado para spec/ quando você executa 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
@@ -8,8 +8,10 @@ require 'shoulda/matchers'
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'simplecov'
-rails_support_path = Rails.root.join('spec', 'support', '**', '*.rb')
-Dir[rails_support_path].each { |file| require file }
+
+# Carrega todos os arquivos de suporte
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |file| require file }
+
 SimpleCov.start
 
 begin
@@ -17,21 +19,47 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
 RSpec.configure do |config|
+  # Inclui helpers personalizados para controller specs
   config.include Request::JsonHelpers, type: :controller
-  config.fixture_path = Rails.root.join('spec/fixtures')
   config.include Request::HeadersHelpers, type: :controller
-  config.include Devise::Test::ControllerHelpers
+
+  # Inclui os helpers do Devise apenas para controller specs
+  config.include Devise::Test::ControllerHelpers, type: :controller
+
+  # Inclui os helpers de integração do Devise para request specs
+  config.include Devise::Test::IntegrationHelpers, type: :request
+
+  # Configuração antes de cada controller spec
   config.before(:each, type: :controller) do
     include_default_accept_headers
   end
-  config.use_transactional_fixtures = true
 
-  config.infer_spec_type_from_file_location!
-
-  config.filter_rails_from_backtrace!
-  config.before(:each, type: :request) do |example|
+  # Configuração antes de cada request spec
+  config.before(:each, type: :request) do
     host! 'api.lvh.me:8080'
   end
+
+  # Caminho para fixtures
+  config.fixture_path = Rails.root.join('spec/fixtures')
+
+  # Usa fixtures transacionais
+  config.use_transactional_fixtures = true
+
+  # Infere o tipo de spec a partir da localização do arquivo
+  config.infer_spec_type_from_file_location!
+
+  # Filtra backtraces do Rails
+  config.filter_rails_from_backtrace!
+
+  # Outras configurações específicas podem ser adicionadas aqui
 end
- 
+
+# Configuração do Shoulda Matchers
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
