1e3fb74ad20a077ddfdf967c53f91207de2218b2
Renzo Tognella De Rosa <renzoderosa.tognella@gmail.com>
2024-11-14 11:43:17 -0300

    Session destroy

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     email { FFaker::Internet.email }
     password { '123456' }
-    password_confirmation { '123456' }
+    password_confirmation { '123456' } 
+    token { FFaker::IdentificationPL.identity_card.downcase }
   end
 end
