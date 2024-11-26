fc9fb6cbb6c7c44025f2b30455d4ff0e13a2224b
Renzo Tognella De Rosa <renzoderosa.tognella@gmail.com>
2024-11-26 13:10:43 -0300

    Adicionado product model

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  let(:product) { FactoryBot.build :product}
+
+  subject { product } 
+
+  it { respond_to(:title) }
+  it { respond_to(:price) }
+  it { respond_to(:description) }
+  it { respond_to(:published) }
+  it { respond_to(:user_id) }
+
+  describe 'validations' do
+    it { should validate_presence_of :title}
+    it { should validate_presence_of :price}
+    it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0)}
+    it { should validate_presence_of :user_id}
+  end
+end
